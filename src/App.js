2aec52719d8893d68f9c180f75734793797f431c
aaronhash <aaron.dungca@outlook.com>
2022-08-01 23:45:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [photo, setPhoto] = useState('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/cat-face_1f431.png');
+	let [description, setDescription] = useState();
+	let [loading, setLoading] = useState(false);
+
+	let clickHandler = () => {
+		setLoading(true);
+		axios.get('https://catfact.ninja/fact').then(res => {
+			setDescription(res.data.fact);
+		})
+		axios.get('https://api.thecatapi.com/v1/images/search?api_key=79888c14-333c-403c-ba4a-a5c7165042c2').then(res => {
+			setPhoto(res.data[0].url);
+			setLoading(false);
+		})
+	}
+
+	return (
+		<div className="App">
+			<div className='content'>
+				<h1>Cat Facts!</h1>
+				{loading ? (
+					<img src='https://c.tenor.com/RVvnVPK-6dcAAAAM/reload-cat.gif' alt='a cat' />
+				) : (
+					<>
+						<img src={photo} alt='a cat' />
+						<p>{description}</p>
+					</>
+				)}
+				<div className='button' onClick={clickHandler}>Meow</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
